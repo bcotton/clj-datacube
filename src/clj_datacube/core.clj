cb810bfed6be4e455d6735ee801a804bf4a57b4b
Bob Cotton <bob.cotton@rallydev.com>
2013-06-18 20:41:02 -0600

    Plumbed through the write sync level and the data type

--- src/clj_datacube/core.clj
+++ src/clj_datacube/core.clj
@@ -1,20 +1,23 @@
 (ns clj-datacube.core
   (:import [java.util.concurrent ConcurrentHashMap]
            [com.urbanairship.datacube Dimension DimensionAndBucketType BucketType Rollup DataCube DataCubeIo SyncLevel DbHarness DbHarness$CommitType WriteBuilder ReadBuilder]
-           [com.urbanairship.datacube.bucketers StringToBytesBucketer HourDayMonthBucketer]
+           [com.urbanairship.datacube.bucketers StringToBytesBucketer HourDayMonthBucketer BigEndianIntBucketer BigEndianLongBucketer BooleanBucketer]
            [com.urbanairship.datacube.idservices HBaseIdService MapIdService CachingIdService]
            [com.urbanairship.datacube.dbharnesses MapDbHarness HBaseDbHarness]
-           [com.urbanairship.datacube.ops LongOp]
+           [com.urbanairship.datacube.ops LongOp IntOp DoubleOp]
            [org.apache.hadoop.hbase HBaseConfiguration]
            [org.apache.hadoop.hbase.client HTablePool])
   (:use [clojure.string :only [join]]))
 
-
 (def read-combine-cas DbHarness$CommitType/READ_COMBINE_CAS)
 
-(def full-sync  SyncLevel/FULL_SYNC)
-(def batch-sync SyncLevel/BATCH_SYNC)
-(def batch-async SyncLevel/BATCH_ASYNC)
+(def full-sync-level  SyncLevel/FULL_SYNC)
+(def batch-sync-level SyncLevel/BATCH_SYNC)
+(def batch-async-level SyncLevel/BATCH_ASYNC)
+
+(def long-deserializer LongOp/DESERIALIZER)
+(def int-deserializer IntOp/DESERIALIZER)
+(def double-deserializer DoubleOp/DESERIALIZER)
 
 (defn map-id-service 
   ([]
@@ -23,11 +26,11 @@
      (CachingIdService. size (MapIdService.) name)))
 
 (defn map-db-harness 
-  ([commit-type]
+  ([commit-type deserializer]
      (MapDbHarness. (ConcurrentHashMap.) 
-                    LongOp/DESERIALIZER commit-type (map-id-service)))
-  ([]
-     (map-db-harness read-combine-cas)))
+                    deserializer commit-type (map-id-service)))
+  ([deserializer]
+     (map-db-harness read-combine-cas deserializer)))
 
 (defn hbase-configuration [zookeeper-connect]
   (doto (HBaseConfiguration/create) 
@@ -42,17 +45,24 @@
                            (.getBytes "spans"))
                      "cache"))
 
-(defn hbase-db-harness []
-  (println "Creating HBase DB Harness")
-  (HBaseDbHarness. 
-   (HTablePool. (hbase-configuration) 10) 
-   (.getBytes "spans")
-   (.getBytes "spans") 
-   (.getBytes "spans") 
-   LongOp/DESERIALIZER 
-   (hbase-id-service) 
-   read-combine-cas))
+(defn hbase-db-harness 
+  ([commit-type deserializer]
+     (println "Creating HBase DB Harness")
+     (HBaseDbHarness. 
+      (HTablePool. (hbase-configuration) 10) 
+      (.getBytes "spans")
+      (.getBytes "spans") 
+      (.getBytes "spans") 
+      deserializer 
+      (hbase-id-service) 
+      commit-type))
+  ([deserializer]
+     (hbase-db-harness read-combine-cas deserializer)))
 
+
+;;
+;; Dimensions
+;;
 (defn string-dimension 
   "Create a dimension suitable for storing String values."
   ([name]
@@ -73,93 +83,120 @@
 (def week-bucker HourDayMonthBucketer/weeks)
 (def hour-bucket HourDayMonthBucketer/hours)
 
+(defn int-dimension 
+  "Create a dimension for by Integers"
+  [name] (Dimension. name (BigEndianIntBucketer.) false 8))
+
+(defn long-dimension 
+  "Create a dimension for Longs"
+  [name] (Dimension. name (BigEndianLongBucketer.) false 8))
+
+(defn boolean-dimension
+  "Create a dimension for booleans"
+  [name] (Dimension. name (BooleanBucketer.) false 1))
+
+;; 
+(defn- make-dim-and-bucket 
+  ([dim bucket-type] 
+     (DimensionAndBucketType. dim bucket-type))
+  ([dim]
+     (dim-and-bucket dim BucketType/IDENTITY)))
+
+(defn- parse-rollup-spec [cube rollup-spec]
+  (let [dimensions (:dimensions cube)] 
+    (loop [spec             rollup-spec
+           dims-and-buckets #{}]
+      (let [dimension (dim cube (first spec))
+            bucket-type (second spec)]
+        (cond
+         (empty? spec) dims-and-buckets
+         (nil? dimension) (throw (IllegalArgumentException. 
+                                  (str "Problem creating Rollups. Dimension " (first spec) " not found in this cube. Known dimensions are " (join " " (keys dimensions)))))
+         (instance? BucketType bucket-type) (recur (rest (rest spec)) 
+                                                   (conj dims-and-buckets 
+                                                         (make-dim-and-bucket dimension bucket-type)))
+         :default (recur (rest spec) (conj dims-and-buckets
+                                        (make-dim-and-bucket dimension))))))))
+
+(defn- dim [cube name]
+  (get (:dimensions cube) name))
+
+;;
+;; Cube DSL
+;; 
 (defn dimension 
   [cube key dim]
   (-> cube 
       (assoc-in [:dimensions key] dim)
       (assoc :dimension-list  (conj (get cube :dimension-list []) key))))
 
-(defn- dim-and-bucket 
-  ([dim bucket-type] 
-     (DimensionAndBucketType. dim bucket-type))
-  ([dim]
-     (dim-and-bucket dim BucketType/IDENTITY)))
-
-(defn- make-dims-and-buckets [cube rollup-spec]
-  (let [dimensions (:dimensions cube)] 
-    (loop [d                rollup-spec
-           dims-and-buckets #{}]
-      (let [dimension (get dimensions (first d))
-            bucket-type (second d)]
-        (cond
-         (empty? d) dims-and-buckets
-         (nil? dimension) (throw (IllegalArgumentException. 
-                                  (str "Problem creating Rollups. Dimension " (first d) " not found in this cube. Known dimensions are " (join " " (keys dimensions)))))
-         (instance? BucketType bucket-type) (recur (rest (rest d)) 
-                                                   (conj dims-and-buckets 
-                                                         (dim-and-bucket dimension bucket-type)))
-         :default (recur (rest d) (conj dims-and-buckets
-                                        (dim-and-bucket dimension))))))))
-
 (defn rollup 
   ([cube dims] 
-     (let [dimensions (:dimensions cube)
-           r (Rollup. (make-dims-and-buckets cube dims))] 
-       (assoc cube :rollups 
-              (conj (:rollups cube) r))))
+     (let [r (Rollup. (parse-rollup-spec cube dims))] 
+       (assoc cube :rollups (conj (:rollups cube) r))))
   ([cube]
      (assoc cube :rollups (conj (:rollups cube) (Rollup. #{})))))
 
-(defn- dim [cube name]
-  (get (:dimensions cube) name))
-
-(defn write-builder [cube dims-and-values]
+;;
+;; Read and Write
+;;
+(defn- write-builder [cube dims-and-values]
   (let [builder (WriteBuilder. (:cube cube))]
     (doseq [[dim-name _ coordinate] dims-and-values]
       (.at builder (dim cube dim-name) coordinate))
     builder))
 
-(defn read-builder [cube dims-and-values]
+(defn- read-builder [cube dims-and-values]
   (let [builder (ReadBuilder. (:cube cube))]
     (doseq [[dim-name bucket-type coordinate] dims-and-values]
       (.at builder (dim cube dim-name) bucket-type coordinate))
     builder))
 
-(defn at 
+(defn at
+  "Sugar for wrapping dimension and buckets."
   ([dim coordinate] (at dim BucketType/IDENTITY coordinate))
-  ([dim ^BucketType bucket-type coordinate] [dim bucket-type coordinate]))
+  ([dim ^BucketType bucket-type coordinate] (vector dim bucket-type coordinate)))
 
-(defn- write-io [io value builder]
-  (.writeSync io (LongOp. value) builder))
+(defn- cube-op [cube value]
+  (let [measure-type (:measure-type cube)]
+    (case measure-type
+      :long (LongOp. value)
+      :int (IntOp. value)
+      :double (DoubleOp. value)
+      (throw (IllegalArgumentException. (str "Unknown measure type " measure-type))))))
 
-(defn- get-io [io builder]
-  (let [value (.get io builder)]
+(defn- write-io [cube value builder]
+  (if (:sync-level cube full-sync-level)
+    (.writeSync (:cubeio cube) (cube-op cube value) builder)    
+    (.writeAsync (:cubeio cube) (cube-op cube value) builder)))
+
+(defn- get-io [cube  builder]
+  (let [value (.get (:cubeio cube) builder)
+        measure-type (:measure-type cube)]
       (if (.isPresent value)
-        (-> (.get value) .getLong)
+        (case measure-type
+          :long (.getLong (.get value))
+          :int (.getInt (.get value))
+          :double (.getDouble (.get value)))
         0)))
 
 (defn write-value [cube value & dims-and-values]
-  (write-io (:cubeio cube) value (write-builder cube dims-and-values)))
+  (write-io cube value (write-builder cube dims-and-values)))
 
 (defn read-value [cube & dims-and-values]
-  (get-io (:cubeio cube) (read-builder cube dims-and-values)))
+  (get-io cube (read-builder cube dims-and-values)))
 
 (defmacro defcube 
   [cube-name measure-type db-harness batch-size flush-interval sync-level & body]
-  `(let [clj-cube# (-> {}
-                       ~@body)
-         cube# (DataCube. (for [dim# (:dimension-list clj-cube#)]
-                            (dim# (:dimensions clj-cube#)))
-                          (:rollups clj-cube#))
-         cubeio# (DataCubeIo. cube# ~db-harness ~batch-size ~flush-interval ~sync-level)
-         cube2# (-> clj-cube#
-                   (assoc :cube cube#)
-                   (assoc :cubeio cubeio#))]
-    (def ~(symbol cube-name) cube2#)))
-
-
-
-
-
-
-
+  `(let [cube# (-> {}
+                   ~@body)
+         data-cube# (DataCube. (for [dim# (:dimension-list cube#)] ;; Dimensions are ordered
+                                 (dim# (:dimensions cube#)))
+                               (:rollups cube#))
+         cubeio# (DataCubeIo. data-cube# ~db-harness ~batch-size ~flush-interval ~sync-level)
+         cube# (-> cube#
+                   (assoc :cube data-cube#)
+                   (assoc :cubeio cubeio#)
+                   (assoc :sync-level ~sync-level)
+                   (assoc :measure-type ~measure-type))]
+     (def ~(symbol cube-name) cube#)))
--- test/clj_datacube/core_test.clj
+++ test/clj_datacube/core_test.clj
@@ -5,9 +5,10 @@
   (:import [com.urbanairship.datacube Dimension Rollup]))
 
 (facts "first cube"
-  (defcube my-cube :long (map-db-harness) 10 1000 full-sync
+  (defcube my-cube :long (map-db-harness long-deserializer) 10 1000 full-sync-level
+
     (dimension :name (string-dimension "name"))
-    (dimension :measure (string-dimension "test"))
+    (dimension :measure (string-dimension "measure"))
     (dimension :time (time-dimension "time" 8))
 
     (rollup)
@@ -25,7 +26,6 @@
     (count (:dimensions my-cube))                                  => 3
     (every? #(instance? Dimension %) (vals (:dimensions my-cube))) => true
     (:dimension-list my-cube)                                      => [:name :measure :time]
-
     (count (:rollups my-cube))                                     => 4
     (every? #(instance? Rollup %) (:rollups my-cube))              => true)
 
@@ -42,7 +42,28 @@
      (read-value my-cube (at :name "name2") (at :time day-bucket (time/date-time 2013 06 01))) => 0
      (read-value my-cube (at :name "name2") (at :time day-bucket (time/date-time 2013 06 02))) => 105
      (read-value my-cube (at :name "name2") (at :time day-bucket (time/date-time 2013 06 03))) => 0
-))
+     ))
+
+(facts "Type checking stored values"
+  (defcube long-cube :long (map-db-harness long-deserializer) 10 1000 full-sync-level (rollup))
+  (defcube int-cube :int (map-db-harness int-deserializer) 10 1000 full-sync-level (rollup))
+  (defcube double-cube :double (map-db-harness double-deserializer) 10 1000 full-sync-level (rollup))
+
+  (write-value long-cube 1000)
+  (write-value int-cube 1000)
+  (write-value double-cube 1000.0)
+
+  (class (read-value long-cube))   => Long
+  (class (read-value int-cube))    => Integer
+  (class (read-value double-cube)) => Double
+  )
+
+(facts "cube rollups on missing dimensions"
+  (defcube broken :long (map-db-harness long-deserializer) 10 1000 full-sync-level
+    (dimension :name (string-dimension "name"))
+    (rollup [:unknown]))  
+  => (throws IllegalArgumentException #"Dimension :unknown not found")
+)
 
 
 
